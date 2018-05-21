bf079c3c80f58e494611e0c7aba541e7f2f9ac42
Philippe Legault <philippe.m.legault96@gmail.com>
2018-05-21 18:03:41 -0400

    first commit

--- 100Words/ViewController.swift
+++ 100Words/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let chinChars = ["的", "人", "我"]
+    @IBOutlet weak var chinWord: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func newWord(_ sender: Any) {
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
